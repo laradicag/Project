2a88a4097271387f7f9fa46d98320f6f11909398
Lara <lmdicag@myseneca.ca>
2023-11-25 10:40:26 -0500

    first commit

---
+++ manipulating.h
@@ -0,0 +1,21 @@
+/*
+Author: Elijah Alton Flores, eaflores1@myseneca.ca, 106480239, 2023-11-20, CPR101, CPR Final Project
+manipulating : Manipulations
+Purpose: Concatenate 2 strings
+*/
+
+// MANIPULATING MODULE HEADER
+#ifndef _MANIPULATING_H_
+#define _MANIPULATING_H_
+
+// Include the Standard Input and Output Library
+#include <stdio.h> 
+
+// Include the String Handling Library
+#include <string.h> 
+
+//Function prototype for manipulating function
+void manipulating(void);
+
+// End of the _MANIPULATING_H_ macro definition check
+#endif 
